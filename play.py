7196dfcd50db8097044ed5f3fe6ff2d8e59f76c2
Akshay Ballal <arballal95@gmail.com>
2023-12-30 19:52:05 +0100

    edit play

--- play.py
+++ play.py
@@ -1,79 +1,72 @@
+import base64
 import torch
 import cv2
 import keyboard
-from PIL import Image, ImageGrab
+from PIL import Image
 import numpy as np
-from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize,  Grayscale
-from tqdm import tqdm
+from torchvision.transforms import (
+    Compose,
+    Resize,
+    CenterCrop,
+    ToTensor,
+    Normalize,
+    Grayscale,
+)
 from torch import nn
 from collections import deque
-from torchvision.models.video import mc3_18
-from matplotlib import pyplot as plt
+from torchvision.models.video import r3d_18
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-class SnakeModel(nn.Module):
-    def __init__(self, in_channels=4, out_channels=32, mlp_size=256, n_actions=5, dropout = 0.2):
-        super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.mlp_size = mlp_size
-        self.n_classes = n_actions
-        self.convblock = nn.Sequential(
-            nn.Conv2d(in_channels, 16, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(16, 32, 4,2),
-            nn.ReLU()
-        )
-        self.classifier =nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=out_channels*9*9, out_features=mlp_size),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(in_features=mlp_size, out_features=n_actions)
-        )
-
-    def forward(self,x):
-        x = self.classifier(self.convblock(x).permute(0,2,3,1))
-        return x
-
-label_keys= {
-    0: "",
-    1 :"left",
-    2: "up",
-    3: "right",
-    4: "down"
-}
+label_keys = {0: "", 1: "left", 2: "up", 3: "right", 4: "down"}
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = mc3_18(weights = None)
+model = r3d_18(weights=None)
 model.fc = nn.Linear(in_features=512, out_features=5, bias=True)
 
 model.load_state_dict(torch.load("model_mc3.pth"))
 model.to(device)
 model.eval()
 
-transformer = Compose([
-    Resize((84,84), antialias=True),
-    CenterCrop(84),
-    ToTensor(),
-    Normalize(mean =[ 0.8725,  1.8742, -0.2931], std =[0.3376, 0.3561, 0.3825] )
-])
+transformer = Compose(
+    [
+        Resize((84, 84), antialias=True),
+        CenterCrop(84),
+        ToTensor(),
+        Normalize(mean=[-0.7138, -2.9883, 1.5832], std=[0.2253, 0.2192, 0.2149]),
+    ]
+)
 
-def generator():
-    while(not keyboard.is_pressed("esc")):
-      yield
+# Create a webdriver instance using Firefox
+driver = webdriver.Firefox()
+# Navigate to the Google Snake game website
+driver.get("https://www.google.com/fbx?fbx=snake_arcade")
 
 frame_stack = deque(maxlen=4)
 
-for _ in tqdm(generator()):
-    image = ImageGrab.grab(bbox = (685,350,1235,840)) 
+while True:
+    # Find the canvas element on the webpage
+    canvas = driver.find_element(By.CSS_SELECTOR, "canvas")
+
+    # Get the base64 encoded image data from the canvas
+    canvas_base64 = driver.execute_script(
+        "return arguments[0].toDataURL('image/png').substring(21);", canvas
+    )
+    # Decode the base64 data to get the PNG image
+    canvas_png = base64.b64decode(canvas_base64)
+
+    # Convert the PNG image to a grayscale numpy array
+    image = cv2.cvtColor(
+        np.array(Image.open(io.BytesIO(canvas_png))), cv2.COLOR_BGR2RGB
+    )
 
     frame_stack.append(transformer(image))
-    input = torch.stack([*frame_stack],dim = 1).to(device).squeeze().unsqueeze(0)
+    input = torch.stack([*frame_stack], dim=1).to(device).squeeze().unsqueeze(0)
 
     if len(frame_stack) == 4:
         with torch.inference_mode():
             outputs = model(input).to(device)
-            preds = torch.softmax(outputs, dim=1).argmax(dim = 1)
+            preds = torch.softmax(outputs, dim=1).argmax(dim=1)
 
             if preds.item() != 0:
                 keyboard.press_and_release(label_keys[preds.item()])
